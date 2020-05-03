980cd39b665235c7d4fdb5a31e618e3d36c5999a
goran-bikic <goranbikich@gmail.com>
2020-05-03 15:42:55 +0200

    Peer-graded Assignment: Programming Assignment 2: Lexical Scoping
    - Caching the Inverse of a Matrix
       -makeCacheMatrix
    cacheSolve:

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function, makeCacheMatrix creates a special matrix object.
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    inc <<- NULL
+    
+  }
+get <- function() x
+setInverse <- function(inverse) inv <<- inverse
+getInverse <- function() inv 
+list(set =set,
+     get = get,
+     setInverse = setInverse,
+     getInverse = getInverse)
+  
 }
 
 
-## Write a short comment describing this function
+## Next function, cacheSolve, computes the inverse of the special "matrix" created by makeCacheMatrix function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    message("Getting chaced matrix")
+    return(inv)
+    
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
+  
 }
+
+## Reason why we use this type of functions is because matrix inversion is usually a costly computation
+## and there may be some benefit to caching the inverse of a matrix rather than compute it repeatedly.
